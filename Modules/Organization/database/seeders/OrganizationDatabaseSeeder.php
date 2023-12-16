1730383e80feebf43c8b4197a6f526819344b64b
DanOps! <odehdaniel1@gmail.com>
2023-12-16 18:12:34 +0100

    Adding application modules

---
+++ Modules/Organization/database/seeders/OrganizationDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Organization\database\seeders;
+
+use Illuminate\Database\Seeder;
+
+class OrganizationDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
